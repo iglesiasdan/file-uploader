ff81b5c01f59b432779c61eec4abc1b88b08790f
Daniel Iglesias <iglesias.dan@gmail.com>
2017-05-09 16:26:15 -0400

    fix origin

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ var fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+
+	res.header("Access-Control-Allow-Origin", "*");
+	// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+});
+
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
